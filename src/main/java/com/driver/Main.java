95a839da2d8253598b3fbf0876805053f64cca25
prajwalh02 <99700885+prajwalh02@users.noreply.github.com>
2023-08-09 14:14:21 +0530

    Attempt 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,7 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 class A {
     String meth() {
         return "Invoking method from class A";
@@ -13,9 +15,6 @@ class B extends A {
 }
 public class Main {
     public static void main(String[] args) {
-        A objA = new A();
-        System.out.println(objA.meth());
-
         B objB = new B();
         System.out.println(objB.meth());
     }
